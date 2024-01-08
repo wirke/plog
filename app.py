19341b2950f51cdab4330a6d7c9715ec665e3795
kristinastev <149689425+kristinastev@users.noreply.github.com>
2024-01-08 23:01:42 +0100

    commit 1.0 Kris verzija

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, url_for, request, redirect, session
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+import ast
 import mysql.connector
 import mariadb
 import json
 import html
 
 konekcija = mysql.connector.connect(
-    passwd='2578',
+    passwd='',
     user='root',
     database='plog',
     port=3306,
@@ -16,18 +16,62 @@ konekcija = mysql.connector.connect(
 
 kursor = konekcija.cursor(dictionary=True)
 app = Flask(__name__)
+app.secret_key = "tajni_kljuc_aplikacije"
+bcrypt = Bcrypt(app)
 #############################################################################
-@app.route("/test", methods=['GET', 'POST'])
-def test() -> html:
-    return render_template("/folder/temp.html")
+def ulogovan():
+    if "ulogovani_korisnik" in session:
+        return True
+    else:
+        return False
+def rola():
+    if ulogovan():
+        return ast.literal_eval(session["ulogovani_korisnik"]).pop("rola")
 #############################################################################
 @app.route("/login", methods=['GET', 'POST'])
-def login() -> html:
-    return render_template("login.html")
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    elif request.method == "POST":
+        forma = request.form
+        upit = "SELECT * FROM user WHERE email=%s"
+        vrednost = (forma['emailPolje'],)
+        candidate = request.form['passwordPolje']
 
-@app.route("/register", methods=['GET', 'POST'])
-def register() -> html:
-    return render_template("register.html")
+        try:
+            kursor.execute(upit, vrednost)
+            user = kursor.fetchone()
+
+            if user and bcrypt.check_password_hash(user['sifra'], candidate):
+                session["ulogovani_korisnik"] = str(user)
+                return redirect(url_for("korisnik"))
+            else:
+                return render_template("login.html", error="Nevalidan email ili sifra")
+
+        except Exception as e:
+            print(f"Error: {e}")
+            return render_template("login.html", error="Greska u logovanju")
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    elif request.method == "POST":
+        upit = """ INSERT INTO
+        user(email, sifra, rola, lokacija)
+        VALUES (%s, %s, %s, %s)
+        """
+        password = request.form['passwordPolje']
+        pw_hash = bcrypt.generate_password_hash(password)
+        forma = (request.form['emailPolje'], pw_hash, request.form['rola'], request.form['lokacijaPolje'])
+        kursor.execute(upit,forma)
+        konekcija.commit()
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    session.pop("ulogovani_korisnik", None)
+    return redirect(url_for("login"))
 #############################################################################
 @app.route("/korisnik", methods=['GET', 'POST'])
 def korisnik() -> html:
