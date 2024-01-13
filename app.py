02fa0dafdf15071ecb7c4efb695ab3ba8fa87129
wirke <90754297+wirke@users.noreply.github.com>
2024-01-13 19:57:56 +0100

    1.4

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect, session
+from flask import Flask, render_template, url_for, request, redirect, session, abort
 from flask_bcrypt import Bcrypt
 import ast
 import mysql.connector
@@ -44,7 +44,7 @@ def login():
 
             if user and bcrypt.check_password_hash(user['sifra'], candidate):
                 session["ulogovani_korisnik"] = str(user)
-                return redirect(url_for("korisnik"))
+                return redirect(url_for("pocetna"))
             else:
                 return render_template("login.html", error="Nevalidni kredencijali")
 
@@ -75,78 +75,104 @@ def logout():
 #############################################################################
 @app.route("/test", methods=['GET', 'POST'])
 def test() -> html:
+    if ulogovan() != True or rola() != 'Admin':
+        return render_template("/greska.html")
     return render_template("/test.html")
-#############################################################################
-@app.route("/kupac", methods=['GET', 'POST'])
-def kupac() -> html:
-    return render_template("/kupac/kupac.html")
 
+@app.route("/pocetna", methods=['GET', 'POST'])
+def pocetna() -> html:
+    if ulogovan() != True or (rola() != 'Kupac' or rola() != 'Admin' or rola() != 'Proizvođač' or rola() != 'Logističar'):
+        return render_template("/greska.html")
+    return render_template("/pocetna.html")
+#############################################################################
 @app.route("/kupac/proizvodi", methods=['GET', 'POST'])
 def prikaz_proizvoda() -> html:
+    if ulogovan() != True or (rola() != 'Kupac' or rola() != 'Admin'):
+        return render_template("/greska.html")
     return render_template("/kupac/proizvodi.html")
 
 @app.route("/kupac/proizvod", methods=['GET', 'POST'])
 def naruci_proizvod() -> html:
+    if ulogovan() != True or (rola() != 'Kupac' or rola() != 'Admin'):
+        return render_template("/greska.html")
     return render_template("/kupac/proizvod.html")
 
 @app.route("/kupac/magacini", methods=['GET', 'POST'])
 def prikaz_magacina() -> html:
+    if ulogovan() != True or (rola() != 'Kupac' or rola() != 'Admin'):
+        return render_template("/greska.html")
     return render_template("/kupac/magacini.html")
 
 @app.route("/kupac/magacin", methods=['GET', 'POST'])
 def prelged_magacina() -> html:
+    if ulogovan() != True or (rola() != 'Kupac' or rola() != 'Admin'):
+        return render_template("/greska.html")
     return render_template("/kupac/magacin.html")
 
 @app.route("/kupac/porudzbine", methods=['GET', 'POST'])
 def porudzbine_korisnik() -> html:
+    if ulogovan() != True or (rola() != 'Kupac' or rola() != 'Admin'):
+        return render_template("/greska.html")
     return render_template("/kupac/porudzbine.html")
 #############################################################################
-@app.route("/proizvodjac", methods=['GET', 'POST'])
-def proizvodjac() -> html:
-    return render_template("/proizvodjac/proizvodjac.html")
-
 @app.route("/proizvodjac/novi-proizvod", methods=['GET', 'POST'])
 def novi_proizvod() -> html:
+    if ulogovan() != True or (rola() != 'Proizvođač' or rola() != 'Admin'):
+        return render_template("/greska.html")
     return render_template("/proizvodjac/novi-proizvod.html")
 
 @app.route("/proizvodjac/moji-proizvodi", methods=['GET', 'POST'])
 def porudzbine_proizvoda() -> html:
+    if ulogovan() != True or (rola() != 'Proizvođač' or rola() != 'Admin'):
+        return render_template("/greska.html")
     return render_template("/proizvodjac/moji-proizvodi.html")
 
 @app.route("/proizvodjac/proizvod", methods=['GET', 'POST'])
 def proizvod() -> html:
+    if ulogovan() != True or (rola() != 'Proizvođač' or rola() != 'Admin'):
+        return render_template("/greska.html")
     return render_template("/proizvodjac/proizvod.html")
 
 @app.route("/proizvodjac/magacini", methods=['GET', 'POST'])
 def pregledaj_magacine() -> html:
+    if ulogovan() != True or (rola() != 'Proizvođač' or rola() != 'Admin'):
+        return render_template("/greska.html")
     return render_template("/proizvodjac/magacini.html")
 
 @app.route("/proizvodjac/magacin", methods=['GET', 'POST'])
 def napuni_magacin() -> html:
+    if ulogovan() != True or (rola() != 'Proizvođač' or rola() != 'Admin'):
+        return render_template("/greska.html")
     return render_template("/proizvodjac/magacin.html")
 
 @app.route("/proizvodjac/porudzbine", methods=['GET', 'POST'])
 def pregledaj_porudzbine() -> html:
+    if ulogovan() != True or (rola() != 'Proizvođač' or rola() != 'Admin'):
+        return render_template("/greska.html")
     return render_template("/proizvodjac/porudzbine.html")
 #############################################################################
-@app.route("/logisticar", methods=['GET', 'POST'])
-def logisticar() -> html:
-    return render_template("/logisticar/logisticar.html")
-
 @app.route("/logisticar/moji-magacini", methods=['GET', 'POST'])
 def moji_magacina() -> html:
+    if ulogovan() != True or (rola() != 'Logističar' or rola() != 'Admin'):
+        return render_template("/greska.html")
     return render_template("/logisticar/moji-magacini.html")
 
 @app.route("/logisticar/magacin", methods=['GET', 'POST'])
 def magacin() -> html:
+    if ulogovan() != True or (rola() != 'Logističar' or rola() != 'Admin'):
+        return render_template("/greska.html")
     return render_template("/logisticar/magacin.html")
 
 @app.route("/logisticar/novi-magacin", methods=['GET', 'POST'])
 def novi_magacin() -> html:
+    if ulogovan() != True or (rola() != 'Logističar' or rola() != 'Admin'):
+        return render_template("/greska.html")
     return render_template("/logisticar/novi-magacin.html")
 
 @app.route("/logisticar/porudzbine", methods=['GET', 'POST'])
 def porudzbina_magacin() -> html:
+    if ulogovan() != True or (rola() != 'Logističar' or rola() != 'Admin'):
+        return render_template("/greska.html")
     return render_template("/logisticar/porudzbine.html")
 
 app.run(debug=True)
