f0de4036f8dafb4c5358bb90bc7ccd9d80e64b27
wirke <90754297+wirke@users.noreply.github.com>
2024-01-15 12:47:11 +0100

    1.6

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from flask import Flask, render_template, url_for, request, redirect, session
-from functools import wraps
+from flask import Flask, render_template, url_for, request, redirect, session, flash
 from flask_bcrypt import Bcrypt
-import ast
+from functools import wraps
 import mysql.connector
-import json
 import html
 
 konekcija = mysql.connector.connect(
@@ -20,21 +18,29 @@ app.secret_key = "tajni_kljuc_aplikacije"
 bcrypt = Bcrypt(app)
 #############################################################################
 def ulogovan():
-    if 'korisnik_id' in session:
-        return True
-    else:
-        return False
+    return 'korisnik_id' in session
+
+@app.context_processor
+def utility_log():
+    return dict(ulogovan=ulogovan)
 
 def rola():
+    return session.get('rola', None) if ulogovan() else None
+
+@app.context_processor
+def utility_role():
+    return dict(rola=rola)
+
+@app.context_processor
+def inject_user_data():
+    user_data = None
     if ulogovan():
-        return session.get('rola')
-    return None
-    
-@app.route("/logout")
-def logout():
-    session.pop('korisnik_id', None)
-    session.pop('rola', None)
-    return redirect(url_for('login'))
+        korisnik_id = session.get('korisnik_id')
+        upit = "SELECT * FROM user WHERE id = %s"
+        kursor.execute(upit, (korisnik_id,))
+        korisnik = kursor.fetchone()
+        user_data = korisnik if korisnik else None
+    return dict(user_data=user_data)
 #############################################################################
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -51,15 +57,21 @@ def login():
             user = kursor.fetchone()
 
             if user and bcrypt.check_password_hash(user['sifra'], sifra):
+                print(f"Uspesno logovanje. Email: {user['email']}")
                 session['korisnik_id'] = user['id']
                 session['rola'] = user['rola']
-                return redirect(url_for('pocetna'), error="uspesno ulogovan")
+                flash("Uspesno ste ulogovani!", "success")
+                return redirect(url_for('pocetna'))
             else:
-                return render_template("login.html", error="Nevalidan email ili sifra")
-
+                print("Neuspelo logovanje. Email:", forma['emailPolje'])
+                print("Hash iz baze:", user['sifra'])
+                flash("Nevalidni kredencijali", "error")
+                return redirect(url_for('greska'))
+            
         except Exception as e:
             print(f"Error: {e}")
-            return render_template("login.html", error="Greska u logovanju")
+            flash("Greska u logovanju", "error")
+            return redirect(url_for('greska'))
 
 @app.route("/register", methods=["GET","POST"])
 def register():
@@ -84,6 +96,13 @@ def register():
         kursor.execute(upit,forma)
         konekcija.commit()
         return redirect(url_for("login"))
+    
+
+@app.route("/logout")
+def logout():
+    session.pop('korisnik_id', None)
+    session.pop('rola', None)
+    return redirect(url_for('login'))
 #############################################################################
 def zahteva_dozvolu(roles=[]):
     def omotac(f):
@@ -109,12 +128,6 @@ def zahteva_ulogovanje(f):
 def greska() -> html:
     return render_template("/greska.html")
 #############################################################################
-@app.route("/test", methods=['GET', 'POST'])
-@zahteva_ulogovanje
-@zahteva_dozvolu(roles=['Admin'])
-def test() -> html:
-    return render_template("/test.html")
-
 @app.route("/pocetna", methods=['GET', 'POST'])
 @zahteva_ulogovanje
 @zahteva_dozvolu(roles=['Admin', 'Proizvođač', 'Logističar', 'Kupac'])
@@ -126,89 +139,89 @@ def pocetna() -> html:
 @zahteva_dozvolu(roles=['Admin', 'Kupac'])
 def prikaz_proizvoda() -> html:
     return render_template("/kupac/proizvodi.html")
-# i tako za svaku #
+
 @app.route("/kupac/proizvod", methods=['GET', 'POST'])
+@zahteva_ulogovanje
+@zahteva_dozvolu(roles=['Admin', 'Kupac'])
 def naruci_proizvod() -> html:
-    if ulogovan() != True or ((rola != 'Kupac') or (rola != 'Admin')):
-        return render_template("/greska.html")
     return render_template("/kupac/proizvod.html")
 
 @app.route("/kupac/magacini", methods=['GET', 'POST'])
+@zahteva_ulogovanje
+@zahteva_dozvolu(roles=['Admin', 'Kupac'])
 def prikaz_magacina() -> html:
-    if ulogovan() != True or ((rola != 'Kupac') or (rola != 'Admin')):
-        return render_template("/greska.html")
     return render_template("/kupac/magacini.html")
 
 @app.route("/kupac/magacin", methods=['GET', 'POST'])
+@zahteva_ulogovanje
+@zahteva_dozvolu(roles=['Admin', 'Kupac'])
 def prelged_magacina() -> html:
-    if ulogovan() != True or ((rola != 'Kupac') or (rola != 'Admin')):
-        return render_template("/greska.html")
     return render_template("/kupac/magacin.html")
 
 @app.route("/kupac/porudzbine", methods=['GET', 'POST'])
+@zahteva_ulogovanje
+@zahteva_dozvolu(roles=['Admin', 'Kupac'])
 def porudzbine_korisnik() -> html:
-    if ulogovan() != True or ((rola != 'Kupac') or (rola != 'Admin')):
-        return render_template("/greska.html")
     return render_template("/kupac/porudzbine.html")
 #############################################################################
 @app.route("/proizvodjac/novi-proizvod", methods=['GET', 'POST'])
+@zahteva_ulogovanje
+@zahteva_dozvolu(roles=['Admin', 'Proizvođač'])
 def novi_proizvod() -> html:
-    if ulogovan() != True or ((rola != 'Proizvođač') or (rola != 'Admin')):
-        return render_template("/greska.html")
     return render_template("/proizvodjac/novi-proizvod.html")
 
 @app.route("/proizvodjac/moji-proizvodi", methods=['GET', 'POST'])
+@zahteva_ulogovanje
+@zahteva_dozvolu(roles=['Admin', 'Proizvođač'])
 def porudzbine_proizvoda() -> html:
-    if ulogovan() != True or ((rola != 'Proizvođač') or (rola != 'Admin')):
-        return render_template("/greska.html")
     return render_template("/proizvodjac/moji-proizvodi.html")
 
 @app.route("/proizvodjac/proizvod", methods=['GET', 'POST'])
+@zahteva_ulogovanje
+@zahteva_dozvolu(roles=['Admin', 'Proizvođač'])
 def proizvod() -> html:
-    if ulogovan() != True or ((rola != 'Proizvođač') or (rola != 'Admin')):
-        return render_template("/greska.html")
     return render_template("/proizvodjac/proizvod.html")
 
 @app.route("/proizvodjac/magacini", methods=['GET', 'POST'])
+@zahteva_ulogovanje
+@zahteva_dozvolu(roles=['Admin', 'Proizvođač'])
 def pregledaj_magacine() -> html:
-    if ulogovan() != True or ((rola != 'Proizvođač') or (rola != 'Admin')):
-        return render_template("/greska.html")
     return render_template("/proizvodjac/magacini.html")
 
 @app.route("/proizvodjac/magacin", methods=['GET', 'POST'])
+@zahteva_ulogovanje
+@zahteva_dozvolu(roles=['Admin', 'Proizvođač'])
 def napuni_magacin() -> html:
-    if ulogovan() != True or ((rola != 'Proizvođač') or (rola != 'Admin')):
-        return render_template("/greska.html")
     return render_template("/proizvodjac/magacin.html")
 
 @app.route("/proizvodjac/porudzbine", methods=['GET', 'POST'])
+@zahteva_ulogovanje
+@zahteva_dozvolu(roles=['Admin', 'Proizvođač'])
 def pregledaj_porudzbine() -> html:
-    if ulogovan() != True or ((rola != 'Proizvođač') or (rola != 'Admin')):
-        return render_template("/greska.html")
     return render_template("/proizvodjac/porudzbine.html")
 #############################################################################
 @app.route("/logisticar/moji-magacini", methods=['GET', 'POST'])
+@zahteva_ulogovanje
+@zahteva_dozvolu(roles=['Admin', 'Logističar'])
 def moji_magacina() -> html:
-    if ulogovan() != True or ((rola != 'Logističar') or (rola != 'Admin')):
-        return render_template("/greska.html")
     return render_template("/logisticar/moji-magacini.html")
 
 @app.route("/logisticar/magacin", methods=['GET', 'POST'])
+@zahteva_ulogovanje
+@zahteva_dozvolu(roles=['Admin', 'Logističar'])
 def magacin() -> html:
-    if ulogovan() != True or ((rola != 'Logističar') or (rola != 'Admin')):
-        return render_template("/greska.html")
     return render_template("/logisticar/magacin.html")
 
 @app.route("/logisticar/novi-magacin", methods=['GET', 'POST'])
+@zahteva_ulogovanje
+@zahteva_dozvolu(roles=['Admin', 'Logističar'])
 def novi_magacin() -> html:
-    if ulogovan() != True or ((rola != 'Logističar') or (rola != 'Admin')):
-        return render_template("/greska.html")
     return render_template("/logisticar/novi-magacin.html")
 
 @app.route("/logisticar/porudzbine", methods=['GET', 'POST'])
+@zahteva_ulogovanje
+@zahteva_dozvolu(roles=['Admin', 'Logističar'])
 def porudzbina_magacin() -> html:
-    if ulogovan() != True or ((rola != 'Logističar') or (rola != 'Admin')):
-        return render_template("/greska.html")
     return render_template("/logisticar/porudzbine.html")
 
 if __name__ == "__main__":
