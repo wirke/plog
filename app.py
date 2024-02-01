0c17ef87ef416e4b7d07a1ee991ae10ee884bccf
wirke <90754297+wirke@users.noreply.github.com>
2024-02-01 18:57:51 +0100

    2.7

--- app.py
+++ app.py
@@ -553,29 +553,32 @@ def postoji_proizvod(korisnik_id):
 @zahteva_dozvolu(roles=['Admin', 'Proizvođač'])
 def proizvod(proizvod_id) -> html:
     
-    upit_pr= """SELECT p.id, p.ime, p.kategorija, p.cena, p.opis
-    FROM proizvod p
-    WHERE p.id = %s
-    """
-    kursor.execute(upit_pr, (proizvod_id,))
-    proizvod = kursor.fetchone()
+    if request.method == 'GET':
+        upit_pr= """
+        SELECT p.id, p.ime, p.kategorija, p.cena, p.opis
+        FROM proizvod p
+        WHERE p.id = %s
+        """
+        kursor.execute(upit_pr, (proizvod_id,))
+        proizvod = kursor.fetchone()
 
-    upit_mag = """SELECT s.id, s.ime, s.kapacitet, s.lokacija
-    FROM skladiste s
-    JOIN sadrzi sd ON s.id=sd.skladiste_id
-    WHERE sd.proizvod_id = %s
-    """
-    kursor.execute(upit_mag, (proizvod_id,))
-    skladiste = kursor.fetchall()
+        upit_mag = """
+        SELECT s.id, s.ime, s.kapacitet, s.lokacija
+        FROM skladiste s
+        JOIN sadrzi sd ON s.id=sd.skladiste_id
+        WHERE sd.proizvod_id = %s
+        """
+        kursor.execute(upit_mag, (proizvod_id,))
+        skladiste = kursor.fetchall()
 
-    upit_sva_skladista = """SELECT s.id, s.ime, s.kapacitet - COALESCE(SUM(sd.kolicina), 0) AS dostupni_kapacitet, s.lokacija
-    FROM skladiste s
-    LEFT JOIN sadrzi sd ON s.id = sd.skladiste_id AND sd.proizvod_id = %s
-    GROUP BY s.id
-    HAVING dostupni_kapacitet > 0 OR dostupni_kapacitet IS NULL;
-    """
-    kursor.execute(upit_sva_skladista, (proizvod_id,))
-    sva_skladista= kursor.fetchall()
+        upit_sva_skladista = """
+        SELECT s.id, s.ime, s.kapacitet, s.lokacija
+        FROM skladiste s
+        LEFT JOIN sadrzi sd ON s.id = sd.skladiste_id AND sd.proizvod_id = <id_proizvoda>
+        WHERE sd.proizvod_id IS NULL;
+        """
+        kursor.execute(upit_sva_skladista, (proizvod_id,))
+        sva_skladista= kursor.fetchall()
 
     if request.method == "POST":
         if 'azuriraj_proizvod' in request.form:
@@ -586,8 +589,8 @@ def proizvod(proizvod_id) -> html:
             vrednosti = (request.form['prIme'], request.form['prKategorija'],request.form['prCena'],request.form['prOpis'], proizvod_id)
             kursor.execute(upit, (vrednosti,))
             konekcija.commit()
-            return redirect(url_for('moji_proizvodi'))
-        
+            return redirect(url_for('prenos', proizvod_id=proizvod_id))
+
         elif 'dodavanje' in request.form:
             kap_mag = """
             SELECT s.id, s.ime, s.kapacitet - COALESCE(SUM(sd.kolicina), 0) AS dostupni_kapacitet
@@ -606,12 +609,24 @@ def proizvod(proizvod_id) -> html:
                 """
                 kursor.execute(upit, (proizvod_id, request.form.get('izabrano_skl'), request.form.get('kolicina'),))
                 konekcija.commit()
-                return redirect(url_for('proizvod', proizvod=proizvod))
+                return redirect(url_for('prenos', proizvod_id=proizvod_id))
+
             else:
-                flash('Proizvod prelazi dostupnu kolicinu skladista')
+                flash('Proizvod prelazi dostupnu kolicinu magacina')
                 return redirect(url_for('greska'))
 
+        elif 'brisanje_iz_skladista' in request.form:
+            skladiste_id = request.form['skladiste_id']
+            izbrisi_proizvod_iz_skladista(proizvod_id, skladiste_id, kursor, konekcija)
+            return redirect(url_for('prenos', proizvod_id=proizvod_id))
+
     return render_template("/proizvodjac/proizvod.html", proizvod=proizvod, skladiste=skladiste, sva_skladista=sva_skladista)
+
+@app.route("/proizvodjac/redirect/<int:proizvod_id>", methods=['GET', 'POST'])
+@zahteva_ulogovanje
+@zahteva_dozvolu(roles=['Admin', 'Proizvođač'])
+def prenos(proizvod_id):
+    return redirect(url_for('proizvod', proizvod_id=proizvod_id))
 
 @app.route("/proizvodjac/magacini", methods=['GET', 'POST'])
 @zahteva_ulogovanje
